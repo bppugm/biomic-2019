74e8747d4f21669aa1eb2ba15b30bf5bdf20324b
davieiycode <rafieiy.m@gmail.com>
2019-02-23 13:52:56 +0700

    Edit Google Tag fixes #10

--- javascripts/gtag.js
+++ javascripts/gtag.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'UA-113279197-1');
+gtag('config', 'UA-116679781-1');
